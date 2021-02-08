daab53eaeb47c1920353cf3204b6ea7c845fdc27
Claudia Hill <hillc255@yahoo.com>
2021-02-08 04:17:58 -0500

    update package

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -10,6 +10,6 @@ export const environment = {
     url: 'dev-mg9nhwma.us.auth0.com', // the auth0 domain prefix
     audience: 'coffeeshop', // the audience set for the auth0 app
     clientId: 'fhaUZdOvVQ23tflbXrxAQ51HG7cXik2f', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100' // http://localhost:8100 https://127.0.0.1:8080/login-results the base url of the running ionic application. 
+    callbackURL: 'http://localhost:8100', // http://localhost:8100 https://127.0.0.1:8080/login-results the base url of the running ionic application. 
   }
 };
